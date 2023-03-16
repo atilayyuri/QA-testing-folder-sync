d00d75ddaf87b765fdabe41bf80bb1e6fb3242c4
atilay <tamkan@th-brandenburg.de>
2023-03-16 18:19:42 +0100

    commiting as VS to try the sub level implementation

--- create_directories_and_files.py
+++ create_directories_and_files.py
@@ -20,8 +20,17 @@ class GenerateRandom:
             else:
                 f.write(f' IDENTICAL CONTENT --- {filename}\n')
 
+    def run_class(self, max_depth, max_files, max_dirs):
+
+        path_1 = self.path_source
+        path_2 = self.path_replica
+
+        self.generate_random_directories(path_1, path_2, max_depth, max_files, max_dirs)
+        pass
+
+
     @staticmethod
-    def count_files_dirs(path, num_files, num_dirs):
+    def count_files_dirs(path):
         files = 0
         dirs = 0
         for dirpath, dirnames, filenames in os.walk(path):
@@ -52,7 +61,7 @@ class GenerateRandom:
         """
         # Check all the files in given paths and break if the max files is reached
         if self.count_files_dirs(target_path, num_files, max_dirs)[0] >= num_files:
-            print(f'Maximum file number is by generate_random reached at {target_path}')
+            print(f'Maximum file number is by generate_random_files reached at {target_path}')
             return
 
         for i in range(num_files):
@@ -70,23 +79,28 @@ class GenerateRandom:
             # Write another file with different content but with same filename at path2 based on probability
             dice_roll = random.randint(10, 100)
             if dice_roll > 40:
-                # Check the path2 exists
-                if not os.path.isdir(os.path.join(target_path_2)):
-                    os.makedirs(target_path_2)
-                    with open(os.path.abspath(os.path.join(target_path_2, filename)), 'w') as f:
-                        f.write(content_2)
-                print(
+                # Check if max file is reached
+                if self.count_files_dirs(target_path_2, num_files, max_dirs)[0] < num_files:
+                    # Check if the path exists
+                    if not os.path.isdir(os.path.join(target_path_2)):
+                        os.makedirs(target_path_2)
+                        with open(os.path.abspath(os.path.join(target_path_2, filename)), 'w') as f:
+                            f.write(content_2)
+                        print(
                     f'The file with another content but same path {os.path.join(target_path_2, filename)} has been created')
 
-                self.log(self.path_source, os.path.abspath(os.path.join(target_path, filename)), 'another')
+                    self.log(self.path_source, os.path.abspath(os.path.join(target_path, filename)), 'another')
 
             # Write another file with same content and filename at path2 based on probabality
             dice_roll_2 = random.randint(10, 100)
             if dice_roll_2 > 10:
-                if not os.path.isdir(os.path.join(target_path_2)):
-                    os.makedirs(target_path_2)
-                    copy2(os.path.join(target_path, filename), os.path.join(target_path_2, filename))
-                    print(
+                # Check if max file is reached
+                if self.count_files_dirs(target_path_2, num_files, max_dirs)[0] < num_files:
+                    # Check if the path exists
+                    if not os.path.isdir(os.path.join(target_path_2)):
+                        os.makedirs(target_path_2)
+                        copy2(os.path.join(target_path, filename), os.path.join(target_path_2, filename))
+                        print(
                         f'The file {os.path.join(target_path, filename)} has been copied to {os.path.join(target_path_2, filename)} ')
 
                     self.log(self.path_source, os.path.abspath(os.path.join(target_path, filename)), 'same')
@@ -100,16 +114,19 @@ class GenerateRandom:
 
         if max_depth == 0:
             # Base case, generate random files to destination
+            # Check max file is reached at source
             if self.count_files_dirs(self.path_source, max_files, max_dirs)[0] < max_files:
                 # Max file number is not reached, continue
-                print(f'Continue creating file via folder at {self.path_source}')
-                self._generate_random_files(self.path_source, self.path_replica, random.randint(0, max_files),max_dirs)
+                print(f'Continue creating file via random_dir_method at {self.path_source}')
+                self._generate_random_files(self.path_source, self.path_replica, random.randint(0, max_files), max_dirs)
             else:
                 print(f'Maximum file number is reached at {self.path_source}')
                 # Max file number is reached, swap paths to create files path2
                 if self.count_files_dirs(self.path_replica, max_files, max_dirs)[0] < max_files:
-                    print(f'Continue creating file via folder at {self.path_replica}')
-                    self._generate_random_files(self.path_replica, self.path_replica, random.randint(0, max_files), max_dirs)
+                    print(f'Continue creating file random_dir_method at {self.path_replica}')
+                    #exit()
+                    self._generate_random_files(self.path_replica, self.path_replica, random.randint(0, max_files),
+                                                max_dirs)
 
         else:
             # Iterate over paths to give chance the probabilistic file creation
@@ -126,35 +143,35 @@ class GenerateRandom:
                         print(f'dir creation continue at {path}')
                         os.makedirs(dir_path)
                         print(f'The directory {dir_path} has been crated')
-                        #self.path_source = dir_path
-                        self.generate_random_directories(max_depth - 1, max_files, max_dirs)
-                        # Generate random files under sub directories as well
+                        # self.path_source = dir_path
                         self._generate_random_files(dir_path, os.path.join(copy_path, dir_name),
                                                     random.randint(0, max_files), max_dirs)
+                        self.generate_random_directories(max_depth - 1, max_files, max_dirs)
+                        # Generate random files under sub directories as well
+
                     # If max dir is reached at source path
                     else:
-                        print(f'Maximum dir number is reached at {path}')
+                        print(f'Maximum dir number is reached at {path}, checking {copy_path}')
                         dir_path_2 = os.path.join(copy_path, dir_name)
                         # If max dir is not reached at replica path, continue
                         if self.count_files_dirs(copy_path, max_files, max_dirs)[1] < max_dirs:
 
-                        #self.path_source = copy_path
-                        #self.path_replica = path
+                            # self.path_source = copy_path
+                            # self.path_replica = path
                             print(f'dir creation continue at {copy_path}')
                             os.makedirs(dir_path_2)
                             print(f'The directory {dir_path_2} has been crated')
-                            self.generate_random_directories(max_depth - 1, max_files, max_dirs)
                             self._generate_random_files(dir_path_2, dir_path,
-                                                    random.randint(0, max_files), max_dirs)
+                                                        random.randint(0, max_files), max_dirs)
+                            self.generate_random_directories(max_depth - 1, max_files, max_dirs)
+
+
                         # If max dir is reached at replica path, check whether the max file is reached
-                        else:
-                            if self.count_files_dirs(copy_path, max_files, max_dirs)[0] < max_files:
-                                self._generate_random_files(dir_path_2, dir_path,
-                                                            random.randint(0, max_files), max_dirs)
+                        if self.count_files_dirs(copy_path, max_files, max_dirs)[0] < max_files:
+                            self._generate_random_files(copy_path, dir_path,
+                                                        random.randint(0, max_files), max_dirs)
                         # If max file is not reached at replica+dir_name path, stop directory creation
-                        #if self.count_files_dirs(dir_path_2, max_files, max_dirs)[0] < max_files:
+                        # if self.count_files_dirs(dir_path_2, max_files, max_dirs)[0] < max_files:
                         #    self._generate_random_files(dir_path_2, dir_path,
                         #                                random.randint(0, max_files), max_dirs)
                         #    return
-
-
